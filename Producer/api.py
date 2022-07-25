59ecfb83b274ed8e26f5ae1f69ca2b365960b6a0
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-07-25 14:43:00 +0200

    Issue #3 is now Issue #2

--- Producer/api.py
+++ Producer/api.py
@@ -12,7 +12,7 @@ from fastapi.responses import PlainTextResponse
 app = FastAPI()
 
 successful_tasks = 0
-credentials = pika.PlainCredentials(os.getenv("RABBITMQ_USER", "admin"), os.getenv("RABBITMQ_PASSWORD", "password"))
+credentials = pika.PlainCredentials(os.getenv("RABBITMQ_USER", "guest"), os.getenv("RABBITMQ_PASSWORD", "guest"))
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=(os.getenv("RABBITMQ_HOST", "localhost")),credentials=credentials))
 channel = connection.channel()
 class Task(BaseModel):
