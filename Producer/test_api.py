c2fc00e2a01505e09bb3d6b6f8f06cb1cb90f8e2
Kacper Dworakowski <kacper.dworakowski@icloud.com>
2022-07-26 20:33:23 +0200

    Who knows...

---
+++ Producer/test_api.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from api import app
+
+client = TestClient(app)
+
+def test_add_item():
+    response = client.post(
+        "/add",
+        json={"taskid": "task1234", "description": "Example description", "params": {"test1": "1234","test2": "5678"}}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Task task1234 added",
+    }
